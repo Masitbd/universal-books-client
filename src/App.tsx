c5a97fa961550cf2fde1238ad562293999814911
sarker24 <sarker24@gmail.com>
2023-07-17 16:40:07 +0200

    Set user persistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 
-
 import MainLayout from './layout/MainLayout'
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from './lib/firebase';
+import { useAppDispatch } from './redux/hooks';
+import { setUser } from './redux/features/user/userSlice';
 
 function App() {
+const dispatch = useAppDispatch()
+  onAuthStateChanged(auth, (user) => {
+  if(user){
+ dispatch(setUser(user.email))
+  }
+  
+});
  
  return (
     <>

a49167ac073342f46dc9c947ea41e2ce5fe35238
sarker24 <sarker24@gmail.com>
2023-07-17 14:45:12 +0200

    Add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 
-import './App.css'
+
 import MainLayout from './layout/MainLayout'
 
 function App() {
  
  return (
     <>
-      <h1 className="font-thin">
-      Hello world!
-    </h1> 
-    <MainLayout />
+     <MainLayout />
     </>
   )
 }
